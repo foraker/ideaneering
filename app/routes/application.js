05d6a56e243f6e4d9972595b4aff32c2489b98aa
Jon Evans <jle@foraker.com>
2015-08-03 22:30:44 -0600

    Create User record when people sign in if necessary

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+
 export default Ember.Route.extend({
   beforeModel: function() {
     this.get("session").fetch().catch(function() {});
@@ -6,10 +7,21 @@ export default Ember.Route.extend({
   actions: {
     signIn: function(provider) {
       var _this = this;
-      this.get("session").open("firebase", { provider: provider }, { scope: 'email' }).then(function(data) {
-        console.log(data);
-        _this.transitionTo('ideas');
-      });
+      this.get("session").open("firebase", { provider: provider }).then(
+        function(data) {
+          var user = _this.store.find('user', { googleId: data.currentUser.id });
+          if (!user.any()) {
+            var userData = data.currentUser;
+
+            _this.store.createRecord('user', {
+              googleId: userData.id,
+              name:     userData.displayName,
+              image:    userData.profileImageURL
+            }).save();
+          }
+          _this.transitionTo('ideas');
+        }
+      );
     },
     signOut: function() {
       this.get("session").close();
