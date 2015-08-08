a49ba179ee5a5dfd2b2a45de88fb46c1c834d678
Jon Evans <jle@foraker.com>
2015-08-07 21:23:13 -0600

    Remove unused attributes

--- app/models/idea.js
+++ app/models/idea.js
@@ -1,7 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  uid: DS.attr('string'),
   title: DS.attr('string'),
   description: DS.attr('string'),
   user: DS.belongsTo('user', { async: true })
--- app/models/user.js
+++ app/models/user.js
@@ -3,6 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   image: DS.attr('string'),
-  googleId: DS.attr('string'),
   ideas: DS.hasMany('idea', { async: true })
 });
