9bb1da32eabef3c1db6c6f551c88a6679ddbe928
Jon Evans <jle@foraker.com>
2015-08-17 21:43:25 -0600

    Persist createdAt property for ideas

--- app/models/idea.js
+++ app/models/idea.js
@@ -3,6 +3,11 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   title: DS.attr('string'),
   description: DS.attr('string'),
+  createdAt: DS.attr('date', {
+    defaultValue: function() {
+      return new Date();
+    }
+  }),
   user: DS.belongsTo('user', { async: true }),
   votes: DS.hasMany('vote', { async: true }),
 
