a85e406f27b983a03df97def3caa36c2ab951e80
Jon Evans <jle@foraker.com>
2015-08-25 20:59:30 -0600

    Load vote's associations synchronously

--- app/models/vote.js
+++ app/models/vote.js
@@ -1,6 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  user: DS.belongsTo('user'),
-  idea: DS.belongsTo('idea')
+  user: DS.belongsTo('user', { async: false }),
+  idea: DS.belongsTo('idea', { async: false })
 });
